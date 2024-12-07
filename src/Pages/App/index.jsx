dcf460f054a64cefdd23c81023a556db47e87f7d
alexxandraSalazar <alexandraalazar121205@gmail.com>
2024-12-07 11:49:08 -0600

    :construction:

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,5 +1,10 @@
 //import { useState } from 'react'
 import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import SignIn from '../SignIn'
+import NotFound from '../NotFound'
 
 import '/src/App.css'
 
@@ -8,6 +13,11 @@ function App() {
   return (
     <div>
       <Home/>
+      <MyAccount/>
+      <MyOrder/>
+      <MyOrders/>
+      <SignIn/>
+      <NotFound/>
     </div>
   )
 }
