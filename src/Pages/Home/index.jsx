4b99625227d512b370902efad91d296161aa8962
alexxandraSalazar <alexandraalazar121205@gmail.com>
2024-12-07 11:11:08 -0600

    :tada:

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,9 @@
+function Home() {
+
+    return (
+      <h1 className='bg-orange-400'>home</h1>
+    )
+  }
+  
+  export default Home
+  
---
+++ src/Pages/NotFound/index.jsx
@@ -0,0 +1,9 @@
+function NotFound() {
+
+    return (
+      <h1 className='bg-orange-400'>Not found</h1>
+    )
+  }
+  
+  export default NotFound
+  
